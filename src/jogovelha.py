a8e84fa7a2a38766e74f092dba15cefeef77ec7c
ruas2022 <101440232+ruas2022@users.noreply.github.com>
2023-03-14 23:12:25 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,27 +1,14 @@
-Python 3.3.0 (v3.3.0:bd8afb90ebf2, Sep 29 2012, 10:55:48) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> def inicializar():
+def inicializar():
+  tab = [ ]
+  for i in range(3):
+    linha = [ ]
+    for j in range(3):
+      linha.append(".")
+    tab.append(linha)
+  return tab
 
-tab = [ ]
-
-for i in range(3):
-
-linha = [ ]
-
-for j in range(3):
-
-linha.append(".")
-
-tab.append(linha)
-
-return tab
-
-def main( ):
-
-jogo = inicializar( )
-
-print (jogo)
-
+def main():
+  jogo = inicializar()
+  print(jogo)
 if __name__ == "__main__":
-
-main()
+  main()
